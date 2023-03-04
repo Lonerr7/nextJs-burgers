e3868dd891f0094f9af8058b3d66478f73fb3ae0
Lonerr7 <krabik2342@gmail.com>
2023-03-04 11:05:52 +0300

    9. Loading props with getStaticProps in Burgers component

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,4 +1,29 @@
-const Burgers = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:5000/items');
+  const burgers = await res.json();
+
+  return {
+    props: {
+      burgers,
+    },
+  };
+};
+
+interface Burger {
+  name: string;
+  image: string;
+  desc: string;
+  price: number;
+  id: number;
+}
+
+interface Props {
+  burgers: Array<Burger>;
+}
+
+const Burgers: React.FC<Props> = ({ burgers }) => {
+  console.log(burgers);
+
   return (
     <div>
       <h1>Наши бургеры</h1>
