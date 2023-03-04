37ce5b30897dc82ef27149ba836e6b45a720b8e2
Lonerr7 <krabik2342@gmail.com>
2023-03-04 08:02:34 +0300

    7. Added image to main page. Added <Head> tag with title and meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,39 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import s from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <main className={s.container}>
-      <h1 className={`${s.title} font-effect-fire-animation`}>Главная</h1>
-      <p className={s.text}>
-        Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
-        мясо. О других составляющих начинки можно поспорить, ведь это дело
-        вкуса.
-      </p>
-      <p className={s.text}>
-        Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
-        обслуживания, правильная атмосфера заведения.
-      </p>
-      <Link className={s.btn} href="/burgers">
-        Все бургеры
-      </Link>
-    </main>
+    <>
+      <Head>
+        <title>Жирные бургеры | главная страница</title>
+        <meta name="title" content="Жирные бургеры" />
+      </Head>
+      <main className={s.container}>
+        <h1 className={`${s.title} font-effect-fire-animation`}>Главная</h1>
+        <div className={s.mainImage}>
+          <Image
+            src="/fatburger.png"
+            alt="burger"
+            width={400}
+            height={300}
+            priority
+          />
+        </div>
+        <p className={s.text}>
+          Что такое идеальный бургер? Лист свежего салата, мягкие булочки,
+          сочное мясо. О других составляющих начинки можно поспорить, ведь это
+          дело вкуса.
+        </p>
+        <p className={s.text}>
+          Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+          обслуживания, правильная атмосфера заведения.
+        </p>
+        <Link className={s.btn} href="/burgers">
+          Все бургеры
+        </Link>
+      </main>
+    </>
   );
 }
