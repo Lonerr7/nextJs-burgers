9e713a2b249ece4c6204dba3bbd8a56cbd313144
Lonerr7 <krabik2342@gmail.com>
2023-03-04 07:31:33 +0300

    4. Added styles to the whole app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
+import s from '../styles/Home.module.css';
+
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>Главная</h1>
-      </main>
-    </>
+    <main className={s.container}>
+      <h1 className={`${s.title} font-effect-fire-animation`}>Главная</h1>
+      <p className={s.text}>
+        Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
+        мясо. О других составляющих начинки можно поспорить, ведь это дело
+        вкуса.
+      </p>
+      <p className={s.text}>
+        Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+        обслуживания, правильная атмосфера заведения.
+      </p>
+      <Link className={s.btn} href="/burgers">
+        Все бургеры
+      </Link>
+    </main>
   );
 }
