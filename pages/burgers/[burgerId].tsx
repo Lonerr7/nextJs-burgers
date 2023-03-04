78945217e64be88fd1f47e3c0a731515dcddf717
Lonerr7 <krabik2342@gmail.com>
2023-03-04 14:09:33 +0300

    12. Passed data to each burger

--- pages/burgers/[burgerId].tsx
+++ pages/burgers/[burgerId].tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import { Burger } from '.';
+import s from '../../styles/Burgers.module.css';
 
 export const getStaticPaths = async () => {
   const res = await fetch('http://localhost:5000/items');
@@ -14,10 +16,33 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details: React.FC = () => {
+export const getStaticProps = async (context: any) => {
+  const { burgerId } = context.params;
+
+  const res = await fetch(`http://localhost:5000/items/${burgerId}`);
+  const burger: Burger = await res.json();
+
+  return {
+    props: {
+      burger,
+    },
+  };
+};
+
+interface Props {
+  burger: Burger;
+}
+
+const Details: React.FC<Props> = ({ burger }) => {
   return (
-    <div>
-      <h1>Detalis</h1>
+    <div className={s.singleBurger}>
+      <h1>{burger.name}</h1>
+      <div className={s.imageContainer}>
+        <Image src={burger.image} alt={burger.name} width={400} height={300} />
+      </div>
+      <div>
+        <p>{burger.desc}</p>
+      </div>
     </div>
   );
 };
