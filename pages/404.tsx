324f690d6f16f52bc720f6b80951fa6e6cce3f02
Lonerr7 <krabik2342@gmail.com>
2023-03-04 07:48:01 +0300

    6. Added automatic redirect from 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,22 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      router.push('/');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Ой...</h1>
